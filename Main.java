90c5383e483e4e9d006fb2ea3691ef6373eeacbe
LeShadowHub <kevins168@utexas.edu>
2017-02-18 02:02:11 -0600

    Test1

---
+++ Main.java
@@ -0,0 +1,131 @@
+/* WORD LADDER Main.java
+ * EE422C Project 3 submission by
+ * <Student1: Kevin Sheu>
+ * <Student1: ks43639>
+ * <Student1: 16239>
+ * <Student2 Name>
+ * <Student2 EID>
+ * <Student2 5-digit Unique No.>
+ * Slip days used: <0>
+ * Git URL:
+ * Spring 2017
+ */
+
+
+package assignment3;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	// static variables and constants only here.
+	
+	public static void main(String[] args) throws Exception {
+		
+		Scanner kb;	// input Scanner for commands
+		PrintStream ps;	// output file
+		// If arguments are specified, read/write from/to files instead of Std IO.
+		if (args.length != 0) {
+			kb = new Scanner(new File(args[0]));
+			ps = new PrintStream(new File(args[1]));
+			System.setOut(ps);			// redirect output to ps
+		} else {
+			kb = new Scanner(System.in);// default from Stdin
+			ps = System.out;			// default to Stdout
+		}
+		initialize();
+		
+		// TODO methods to read in words, output ladder
+		// Main loop
+		while(true){
+			ArrayList<String> inputList = new ArrayList<String>();
+			inputList = parse(kb);
+			
+			// Checking for /quit command
+			if(inputList.isEmpty()){
+				break;
+			}
+			
+			// Testing
+			printLadder(inputList);
+			// End Testing
+			
+			// TODO ladder handling code
+		}
+		// End of Main Loop
+		
+	}
+	
+	public static void initialize() {
+		// initialize your static variables or constants here.
+		// We will call this method before running our JUNIT tests.  So call it 
+		// only once at the start of main.
+	}
+	
+	/**
+	 * @param keyboard Scanner connected to System.in
+	 * @return ArrayList of 2 Strings containing start word and end word. 
+	 * If command is /quit, return empty ArrayList. 
+	 */
+	public static ArrayList<String> parse(Scanner keyboard) {
+		ArrayList<String> start_end = new ArrayList<String>();
+		String start = keyboard.next().toLowerCase();
+		
+		// Handles /quit command
+		if(start.equals("/quit")){
+			return start_end;
+		} else{
+			start_end.add(start);
+			start_end.add(keyboard.next().toLowerCase());
+			return start_end;
+		}
+	}
+	
+	public static ArrayList<String> getWordLadderDFS(String start, String end) {
+		
+		// Returned list should be ordered start to end.  Include start and end.
+		// Return empty list if no ladder.
+		// TODO some code
+		Set<String> dict = makeDictionary();
+		// TODO more code
+		
+		return null; // replace this line later with real return
+	}
+	
+    public static ArrayList<String> getWordLadderBFS(String start, String end) {
+		
+		// TODO some code
+		Set<String> dict = makeDictionary();
+		// TODO more code
+		
+		return null; // replace this line later with real return
+	}
+    
+	public static Set<String>  makeDictionary () {
+		Set<String> words = new HashSet<String>();
+		Scanner infile = null;
+		try {
+			infile = new Scanner (new File("five_letter_words.txt"));
+		} catch (FileNotFoundException e) {
+			System.out.println("Dictionary File not Found!");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		while (infile.hasNext()) {
+			words.add(infile.next().toUpperCase());
+		}
+		return words;
+	}
+	
+	/**
+	 * Prints the ladder with a newline between each entry
+	 * @param ladder
+	 */
+	public static void printLadder(ArrayList<String> ladder) {
+		for(int i = 0; i < ladder.size(); i++){
+			System.out.println(ladder.get(i));
+		}
+	}
+	// TODO
+	// Other private static methods here
+}
